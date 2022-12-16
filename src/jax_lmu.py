f9b611b9e4d81f9a739a107faca9144c86f21077
Élise Zhang <46126244+elisejiuqizhang@users.noreply.github.com>
2022-12-16 14:11:01 -0500

    Update jax_lmu.py

--- src/jax_lmu.py
+++ src/jax_lmu.py
@@ -1,19 +1,38 @@
+from functools import partial
+from typing import Any, Callable, Mapping, Optional, Sequence, Tuple, Union
+
 import numpy as np
 import jax
 from jax import lax, random, numpy as jnp
 from jax import grad, jit, vmap, value_and_grad
 
-from typing import Any, Callable, Sequence
-
 from flax import linen as nn
 
 from scipy.signal import cont2discrete
 
-#---------------------- One LMU Cell ----------------------#
-class LMUCell(nn.Module):
-    """ 
-    LMU Cell
+#---------------------- Cell Base ----------------------#
+# The other classes inherit from this class
 
+class RecurrentCellBase(nn.Module):
+  """Recurrent cell base class."""
+
+  @staticmethod
+  def initialize_carry(batch_dims, size, init_fn=nn.initializers.zeros):
+    """Initialize the RNN cell carry.
+
+    Args:
+      batch_dims: a tuple providing the shape of the batch dimensions.
+      size: the size or number of features of the memory.
+      init_fn: initializer function for the carry.
+    Returns:
+      An initialized carry for the given cell.
+    """
+    raise NotImplementedError
+
+#---------------------- One LMU Cell ----------------------#
+class LMUCell(RecurrentCellBase):
+    """LMU Cell
+    
     Parameters:
         input_size (int) : 
             Size of the input vector (x_t)
@@ -22,8 +41,16 @@ class LMUCell(nn.Module):
         memory_size (int) :
             Size of the memory vector (m_t)
         theta (int) :
-            The number of timesteps in the sliding window that is represented using the LTI system
-
+            The number of timesteps in the sliding window that is represented using the LTI system  
+    
+    Other Attributes:
+        State Space Matrices:
+            A: [memory_size, memory_size]
+            B: [memory_size, 1]
+        Initializers:
+            init_lecun_uni: lecun_normal, for two encoding vectors, e_x and e_h
+            init_zero: constant(0), for the encoding vector e_m
+            init_xav_norm: xavier_normal, for three kernel matrices, W_x, W_h, W_m
     """
 
     input_size: int
@@ -35,91 +62,126 @@ class LMUCell(nn.Module):
     init_zero: Callable = nn.initializers.constant(0.)
     init_xav_norm: Callable = nn.initializers.xavier_normal()
 
+    activation_fn: Callable = nn.tanh
+
     def setup(self):
-        """ Initialize the parameters 
-
-        Trainable parameters:
-            Encoding vectors:
-                e_x: [1, input_size]
-                e_h: [1, hidden_size]
-                e_m: [1, memory_size]
-            Kernel matrices:
-                W_x: [hidden_size, input_size]
-                W_h: [hidden_size, hidden_size]
-                W_m: [hidden_size, memory_size]
-
-        A and B are fixed state space matrices:
-            A: [memory_size, memory_size]?
-            B: [memory_size, 1]?
-        
-        """
-        self.e_x=self.param('e_x', self.init_lecun_uni, (1,self.input_size))
-        self.e_h=self.param('e_h', self.init_lecun_uni, (1,self.hidden_size))
-        self.e_m=self.param('e_m', self.init_zero, (1,self.memory_size))
-        self.W_x=self.param('W_x', self.init_xav_norm, (self.hidden_size,self.input_size))
-        self.W_h=self.param('W_h', self.init_xav_norm, (self.hidden_size,self.hidden_size))
-        self.W_m=self.param('W_m', self.init_xav_norm, (self.hidden_size,self.memory_size))
         self.A, self.B = self.stateSpaceMatrices(self.memory_size, self.theta)
-        
-
-    @staticmethod
-    def initialize_state(batch_size, hidden_size, memory_size):
-        return jnp.zeros((batch_size, hidden_size)), jnp.zeros((batch_size, memory_size))
 
     @nn.compact
-    def __call__(self, x, state):
+    def __call__(self, carry, x):
         """
-        Inputs:
-            x (array) : 
-                Input vector of size [batch_size, input_size]
-            state (array) : 
-                The hidden state:
-                h: [batch_size, hidden_size]
-                m: [batch_size, memory_size]
-
+        Args:
+            carry (tuple): the previous state:
+                h: the hidden vector, [batch_size, hidden_size]
+                m: the memory vector, [batch_size, memory_size]
+                
+            x (array): the input vector,[batch_size, input_size]
+                
         """
-
 
         # Unpack the hidden state and memory
-        h, m = state
+        h, m = carry
 
-        # Eq (7) of the paper
+        # Eq (7) of the paper, compute the input signal u
         # u: [batch_size, 1]
-        u1=self.vmap_matmul(x,self.e_x.T)
-        u2=self.vmap_matmul(h,self.e_h.T)
-        u3=self.vmap_matmul(m,self.e_m.T)
-        u=u1+u2+u3
+        u_x = nn.Dense(features=1,
+                        use_bias=False,
+                        kernel_init=self.init_lecun_uni,
+                        bias_init=self.init_zero,
+                        param_dtype=jnp.float32)
+            # partial(nn.Dense, 
+            #             features=self.input_size,
+            #             use_bias=False,
+            #             kernel_init=self.init_lecun_uni,
+            #             bias_init=self.init_zero,
+            #             param_dtype=jnp.float32)
+        u_h = nn.Dense(features=1,
+                        use_bias=False,
+                        kernel_init=self.init_lecun_uni,
+                        bias_init=self.init_zero,
+                        param_dtype=jnp.float32)
+            # partial(nn.Dense,
+            #             features=self.hidden_size,
+            #             use_bias=False,
+            #             kernel_init=self.init_lecun_uni,
+            #             bias_init=self.init_zero,
+            #             param_dtype=jnp.float32)
+        u_m = nn.Dense(features=1,
+                        use_bias=False,
+                        kernel_init=self.init_zero,
+                        bias_init=self.init_zero,
+                        param_dtype=jnp.float32)
+            # partial(nn.Dense,
+            #             features=self.memory_size, 
+            #             use_bias=False,
+            #             kernel_init=self.init_zero,
+            #             bias_init=self.init_zero,
+            #             param_dtype=jnp.float32)
+        u = u_x(x) + u_h(h) + u_m(m)
 
-        # Eq (4) of the paper
+
+        # Eq (4) of the paper, compute the memory
         # m: [batch_size, memory_size]
-        m1=self.vmap_matmul(m,self.A.T)
-        m2=self.vmap_matmul(u,self.B.T)
-        m=m1+m2
+        m_m = jnp.matmul(m, self.A) 
+        m_u = jnp.matmul(u, self.B.T)
+        new_m= m_m + m_u
 
-        # Eq (6) of the paper
+        # Eq (6) of the paper, compute the hidden state
         # h: [batch_size, hidden_size]
-        h1=self.vmap_matmul(x,self.W_x.T)
-        h2=self.vmap_matmul(h,self.W_h.T)
-        h3=self.vmap_matmul(m,self.W_m.T)
-        h=nn.tanh(h1+h2+h3)
+        h_x = nn.Dense(features=self.hidden_size,
+                        use_bias=False,
+                        kernel_init=self.init_xav_norm,
+                        bias_init=self.init_zero,
+                        param_dtype=jnp.float32)
+            # partial(nn.Dense,
+            #             features=self.hidden_size,
+            #             use_bias=False,
+            #             kernel_init=self.init_xav_norm,
+            #             bias_init=self.init_zero,
+            #             param_dtype=jnp.float32)
+        h_h = nn.Dense(features=self.hidden_size,
+                        use_bias=False,
+                        kernel_init=self.init_xav_norm,
+                        bias_init=self.init_zero,
+                        param_dtype=jnp.float32)
+            # partial(nn.Dense,
+            #             features=self.hidden_size,
+            #             use_bias=False,
+            #             kernel_init=self.init_xav_norm,
+            #             bias_init=self.init_zero,
+            #             param_dtype=jnp.float32)
+        h_m = nn.Dense(features=self.hidden_size,
+                        use_bias=False,
+                        kernel_init=self.init_xav_norm,
+                        bias_init=self.init_zero,
+                        param_dtype=jnp.float32)
+            # partial(nn.Dense,
+            #             features=self.hidden_size,
+            #             use_bias=False,
+            #             kernel_init=self.init_xav_norm,
+            #             bias_init=self.init_zero,
+            #             param_dtype=jnp.float32)
+        new_h = self.activation_fn(h_x(x) + h_h(h) + h_m(m))
+        return (new_h, new_m), new_h
 
+    @staticmethod
+    def initialized_carry(batch_size, hidden_size, memory_size):
+        """Initialize the LMU cell carry.
+        Args:
+            batch_size (int) : 
+                Size of the batch
+            hidden_size (int) : 
+                Size of the hidden vector (h_t)
+            memory_size (int) :
+                Size of the memory vector (m_t)
+        Returns:
+            An initialized carry for the given cell.
+        """
+        h = jnp.zeros((batch_size, hidden_size))
+        m = jnp.zeros((batch_size, memory_size))
         return h, m
 
-    def vmap_matmul(self, input, params):
-        """Following the example from vmap documentation: 
-        https://jax.readthedocs.io/en/latest/_autosummary/jax.vmap.html
-
-        Dimensions:
-            input: [b,a] - [batch_size, input_size]
-            params: [a,c] - [input_size, ... (anything) ]
-            output: [b,c] - [batch_size, ... (anything) ]
-        
-        """
-        vv=lambda x, y: jnp.dot(x, y)  #  ([a], [a]) -> []
-        mv=vmap(vv, in_axes=(0, None), out_axes=0)  #  ([b,a], [a]) -> [b] 
-        mm=vmap(mv, in_axes=(None, 1), out_axes=1)  #  ([b,a], [a,c]) -> [b,c]
-        return mm(input, params)
-
+    
 
     def stateSpaceMatrices(self, memory_size, theta):
         """ Returns the discretized state space matrices A and B """
@@ -143,12 +205,12 @@ class LMUCell(nn.Module):
             
         return A, B
 
-    
+
 #---------------------- LMU Layer ----------------------#
 class LMU(nn.Module):
-    """ 
+    """
     The LMU Layer
-    
+
     Parameters:
         input_size (int) : 
             Size of the input vector (x_t)
@@ -159,7 +221,9 @@ class LMU(nn.Module):
         theta (int) :
             The number of timesteps in the sliding window that is represented using the LTI system
         pmnist (boolean) :
-            Uses different parameter initializers when training on psMNIST (as specified in the paper)
+            Uses different parameter initializers when training on psMNIST (as specified in the paper),
+            Set to false by default
+    
     """
 
     input_size: int
@@ -168,46 +232,29 @@ class LMU(nn.Module):
     theta: int
     pmnist: bool = False
 
-    def setup(self):
-        self.cell = LMUCell(self.input_size, self.hidden_size, self.memory_size, self.theta)
-
     @nn.compact
-    def __call__(self, x, state=None):
+    def __call__(self, carry, x):
         """
         Parameters:
-            x (array):
-                Input vector of size [batch_size, seq_len, input_size]
-            state (array): set to None by default
-                h: [batch_size, hidden_size]
-                m: [batch_size, memory_size]
-        
+            carry (tuple): the previous state, set to None initially
+                h: the hidden vector, [batch_size, hidden_size]
+                m: the memory vector, [batch_size, memory_size]
+            x (array): the input vector, [batch_size, seq_len, input_size]
         """
-        # Assume the order of dimensions is [batch_size, seq_len, ......]
-        batch_size = x.shape[0]
-        seq_len = x.shape[1]
 
-        # Initial State (h_0, m_0)
-        if state is None:
-            state=self.cell.initialize_state(batch_size, self.hidden_size, self.memory_size)
-            # h_0 = jnp.zeros((batch_size, self.hidden_size))
-            # m_0 = jnp.zeros((batch_size, self.memory_size))
-            # state = (h_0, m_0)     
+        LMULayer = nn.scan(LMUCell,
+                            variable_broadcast="params",
+                            split_rngs={"params": False},
+                            in_axes=1,
+                            out_axes=1)
 
-        # Operation over time steps
-        output = []
-        for t in range(seq_len):
-            x_t=x[:,t,:] # x_t: [batch_size, input_size]
-            h_t, m_t = self.cell(x_t, state) # h_t: [batch_size, hidden_size], m_t: [batch_size, memory_size]
-            state=(h_t, m_t)
-            output.append(h_t)
+        return LMULayer(self.input_size, self.hidden_size, self.memory_size, self.theta)(carry, x)
+    
+    @staticmethod
+    def initialize_carry(batch_size, hidden_size, memory_size):
+        return LMUCell.initialized_carry(batch_size, hidden_size, memory_size)
 
-        output = jnp.stack(output) # output: [seq_len, batch_size, hidden_size]
-        output = output.transpose(1, 0, 2) # output: [batch_size, seq_len, hidden_size]
-
-        return output, state
-
-#---------------------- LMU Model ----------------------#
-
+#----------------- LMU Model for pMNIST Classification ----------------#
 class Model(nn.Module):
     input_size: int
     output_size: int
@@ -215,12 +262,18 @@ class Model(nn.Module):
     memory_size: int
     theta: int
 
-    def setup(self):
-        self.lmu = LMU(self.input_size, self.hidden_size, self.memory_size, self.theta)
-        self.classifier = nn.Dense(self.output_size)
-
     @nn.compact
     def __call__(self, x):
-        _, (h_n, _) = self.lmu(x) # [batch_size, hidden_size]
-        output = self.classifier(h_n)
-        return output # [batch_size, output_size]
+
+        # Initialize the carry at the first time step
+        # Get initial carry/state (h_0, m_0)
+        init_carry = LMU.initialize_carry(x.shape[0], self.hidden_size, self.memory_size)
+
+        # Run the LMU layer
+        # h_n: [batch_size, hidden_size]
+        (h_n,_),_= LMU(self.input_size, self.hidden_size, self.memory_size, self.theta)(init_carry, x)
+
+        # Run the output layer
+        output=nn.Dense(features=self.output_size)(h_n)
+
+        return output
